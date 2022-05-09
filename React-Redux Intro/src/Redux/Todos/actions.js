59f931bc085fb5fb0719476fe67ed148f4e3fb42
avinashkvar <98077343+avinashkvar@users.noreply.github.com>
2022-05-09 10:29:11 +0530

    react-redux-intro

---
+++ React-Redux Intro/src/Redux/Todos/actions.js
@@ -0,0 +1,36 @@
+//  Action types
+
+export const ADD_TODO = "ADD_TODO";
+
+export const DELETE_TODO = "DELETE_TODO";
+
+export const TOGGLE_TODO = "TOGGLE_TODO";
+
+export const STORE_DATA = "STORE_DATA";
+//action creators for
+
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload: payload,
+  };
+};
+
+export const storeData = (payload) => {
+  return {
+    type: STORE_DATA,
+    payload: payload,
+  };
+};
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload: payload,
+  };
+};
+export const toggleTodo = (payload) => {
+  return {
+    type: TOGGLE_TODO,
+    payload: payload,
+  };
+};
---
+++ React-Redux Intro/src/Redux/store.js
@@ -0,0 +1,14 @@
+
+import { legacy_createStore as createStore } from "redux";
+import { todoReducer } from "./Todos/reducer";
+
+let init = {
+    todos:[]
+}
+
+
+
+
+export const store = createStore(todoReducer, init);
+
+// store.subscribe(()=>console.log(store.getState()))
